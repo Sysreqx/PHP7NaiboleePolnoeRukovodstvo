0832d42540cd4390d5581190d6ee0cb495ac6146
Kaisar Yerdenbekov <sysreqx@gmail.com>
2021-11-17 23:00:00 +0600

    destructor

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Листинг 22.1. Пример класса. Файл Math/Complex.php</title>
+    <title></title>
     <style type="text/css">
         body {
           background-color: grey;
@@ -12,12 +12,7 @@
 </head>
 <body>
 <?php 
-require_once "Math/Complex2.php";
-$a = new MathComplex2(314, 101);
-$a->add(new MathComplex2(303, 6));
-echo $a;
-
-
+    require_once "refcount.php";
 ?>
 </body>
 </html>
